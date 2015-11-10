8a35ce6ba89af0fceb45fce6800801dc7938d34d
小丛 <lj105021@alibaba-inc.com>
2015-11-10 17:10:51 +0800

    add notes

---
+++ notes_config.js
@@ -0,0 +1,39 @@
+var DATA = [
+	{
+		date: "20151014",
+		title: "arguments.callee",
+		tag: ""
+	},
+	{
+		date: "20151023",
+		title: "height 100%",
+		tag: ""
+	},
+	{
+		date: "20151024",
+		title: "如何在 Iterm2 里，用 sublime 打开文件",
+		tag: "command,js"
+	},
+	// 11
+	{
+		date: "20151103",
+		title: "前端工具",
+		tag: ""
+	},
+	{
+		date: "20151104",
+		title: "转义",
+		tag: ""
+	},
+	{
+		date: "20151109",
+		title: "自己的 Github page 的 UV 过低？",
+		tag: ""
+	},
+	{
+		date: "20151109",
+		title: "开发环境的一些东西，记一记",
+		tag: ""
+	}
+];
+
