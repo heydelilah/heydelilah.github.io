902e1e44c2a8eb0f68f5a5006eb39190553c39a3
delilah <justworm@163.com>
2018-07-15 11:26:26 +0800

    init

---
+++ js/catalogue.js
@@ -0,0 +1,9 @@
+var CATALOGUE = [
+	{
+		title: "相对定位",
+		url: "https://github.com/heydelilah/heydelilah.github.io/blob/master/posts/2014-05-08-%E7%9B%B8%E5%AF%B9%E5%AE%9A%E4%BD%8D.md",
+		tags: "js",
+		img: "",
+		date: 20140508
+	}
+];
---
+++ todo.md
@@ -0,0 +1,4 @@
+todo
+
+
+update post img urls
